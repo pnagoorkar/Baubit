bfc11ccb60e3d024aa6af177aeb1d3ad80d06bfe
prash <prashant.nagoorkar@gmail.com>
2024-09-19 10:49:16 -0400

    Removed unnecessary code

--- Baubit/DI/InitializeServiceProviderFactoryRegistrar.cs
+++ Baubit/DI/InitializeServiceProviderFactoryRegistrar.cs
@@ -1,25 +1,25 @@
-﻿using FluentResults;
-using Microsoft.Extensions.Configuration;
+﻿//using FluentResults;
+//using Microsoft.Extensions.Configuration;
 
-namespace Baubit.DI
-{
-    public static partial class Operations
-    {
-        public static async Task<Result<IServiceProviderFactoryRegistrar>> InitializeServiceProviderFactoryRegistrarAsync(ServiceProviderFactoryRegistrarInitializationContext context)
-        {
-            var serviceProviderMetaFactoryTypeAQN = context.Configuration["serviceProviderFactoryRegistrar"];
-            var serviceProviderMetaFactoryConcreteType = Type.GetType(serviceProviderMetaFactoryTypeAQN!);
-            var serviceProviderMetaFactory = (IServiceProviderFactoryRegistrar)Activator.CreateInstance(serviceProviderMetaFactoryConcreteType!)!;
-            return Result.Ok(serviceProviderMetaFactory);
-        }
-    }
+//namespace Baubit.DI
+//{
+//    public static partial class Operations
+//    {
+//        public static async Task<Result<IServiceProviderFactoryRegistrar>> InitializeServiceProviderFactoryRegistrarAsync(ServiceProviderFactoryRegistrarInitializationContext context)
+//        {
+//            var serviceProviderMetaFactoryTypeAQN = context.Configuration["serviceProviderFactoryRegistrar"];
+//            var serviceProviderMetaFactoryConcreteType = Type.GetType(serviceProviderMetaFactoryTypeAQN!);
+//            var serviceProviderMetaFactory = (IServiceProviderFactoryRegistrar)Activator.CreateInstance(serviceProviderMetaFactoryConcreteType!)!;
+//            return Result.Ok(serviceProviderMetaFactory);
+//        }
+//    }
 
-    public class ServiceProviderFactoryRegistrarInitializationContext
-    {
-        public IConfiguration Configuration { get; init; }
-        public ServiceProviderFactoryRegistrarInitializationContext(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-    }
-}
+//    public class ServiceProviderFactoryRegistrarInitializationContext
+//    {
+//        public IConfiguration Configuration { get; init; }
+//        public ServiceProviderFactoryRegistrarInitializationContext(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+//    }
+//}
--- BaubitCLI/Program.cs
+++ BaubitCLI/Program.cs
@@ -1,4 +1,3 @@
-﻿Baubit.Application.Initialize();
-args = ["host", @"hostSettings.json"];
+﻿args = ["host", @"hostSettings.json"];
 var result = Baubit.CLI.Operations.RunCLIOperationAsync(new Baubit.CLI.CLIOperationContext(args)).GetAwaiter().GetResult();
 Environment.Exit(result.IsSuccess == true ? 0 : 1);
