a4417c5d6bf6f325297872b92994831981d3e085
prash <prashant.nagoorkar@gmail.com>
2024-10-09 09:41:50 -0400

    WIP - Killing the baubit package store :-(

--- Baubit/Store/CSProjBuilder.cs
+++ Baubit/Store/CSProjBuilder.cs
@@ -1,35 +1,35 @@
-﻿using Baubit.Process;
+﻿//using Baubit.Process;
 
-namespace Baubit.Store
-{
-    public class CSProjBuilder : AProcess
-    {
-        public CSProjBuilder((string, IEnumerable<string>) args) : base(args.Item1, args.Item2)
-        {
-        }
+//namespace Baubit.Store
+//{
+//    public class CSProjBuilder : AProcess
+//    {
+//        public CSProjBuilder((string, IEnumerable<string>) args) : base(args.Item1, args.Item2)
+//        {
+//        }
 
-        public CSProjBuilder(string csProjFile, string buildOutputFolder) : this(BuildArguments(csProjFile, buildOutputFolder))
-        {
+//        public CSProjBuilder(string csProjFile, string buildOutputFolder) : this(BuildArguments(csProjFile, buildOutputFolder))
+//        {
 
-        }
+//        }
 
-        private static (string, IEnumerable<string>) BuildArguments(string csProjFile, string buildOutputFolder)
-        {
-            string fileName = "dotnet";
-            IEnumerable<string> arguments = ["build", csProjFile,
-                                             "--configuration", "Debug",
-                                             "--output", buildOutputFolder];
-            return (fileName, arguments);
-        }
+//        private static (string, IEnumerable<string>) BuildArguments(string csProjFile, string buildOutputFolder)
+//        {
+//            string fileName = "dotnet";
+//            IEnumerable<string> arguments = ["build", csProjFile,
+//                                             "--configuration", "Debug",
+//                                             "--output", buildOutputFolder];
+//            return (fileName, arguments);
+//        }
 
-        protected override void HandleError(StreamReader standardError)
-        {
-            var error = standardError.ReadToEnd();
-        }
+//        protected override void HandleError(StreamReader standardError)
+//        {
+//            var error = standardError.ReadToEnd();
+//        }
 
-        protected override void HandleOutput(StreamReader standardOutput)
-        {
-            var output = standardOutput.ReadToEnd();
-        }
-    }
-}
+//        protected override void HandleOutput(StreamReader standardOutput)
+//        {
+//            var output = standardOutput.ReadToEnd();
+//        }
+//    }
+//}
--- Baubit/Store/ProjectAssets.cs
+++ Baubit/Store/ProjectAssets.cs
@@ -1,58 +1,58 @@
-﻿using Baubit.Configuration;
-using FluentResults;
-using Microsoft.Extensions.Configuration;
+﻿//using Baubit.Configuration;
+//using FluentResults;
+//using Microsoft.Extensions.Configuration;
 
-namespace Baubit.Store
-{
-    public class ProjectAssets
-    {
-        public IConfiguration Configuration { get; init; }
-        private ProjectAssets(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-        public static Result<ProjectAssets> Read(ConfigurationSource source)
-        {
-            return Result.Try(() => new ProjectAssets(source.Load()));
-        }
-    }
+//namespace Baubit.Store
+//{
+//    public class ProjectAssets
+//    {
+//        public IConfiguration Configuration { get; init; }
+//        private ProjectAssets(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+//        public static Result<ProjectAssets> Read(ConfigurationSource source)
+//        {
+//            return Result.Try(() => new ProjectAssets(source.Load()));
+//        }
+//    }
 
-    public static class ProjectAssetsExtensions
-    {
-        public static IConfigurationSection GetTargetFrameworkTargets(this ProjectAssets projectAssets, string targetFramework)
-        {
-            return projectAssets.Configuration
-                                .GetSection("targets")
-                                .GetChildren()
-                                .FirstOrDefault(child => child.Key.Equals(targetFramework))!;
-        }
+//    public static class ProjectAssetsExtensions
+//    {
+//        public static IConfigurationSection GetTargetFrameworkTargets(this ProjectAssets projectAssets, string targetFramework)
+//        {
+//            return projectAssets.Configuration
+//                                .GetSection("targets")
+//                                .GetChildren()
+//                                .FirstOrDefault(child => child.Key.Equals(targetFramework))!;
+//        }
 
-        public static Package BuildPackage(this ProjectAssets projectAssets, string assemblyName, string targetFramework)
-        {
-            return projectAssets.BuildSerializablePackages(assemblyName, targetFramework)
-                                .AsPackages()
-                                .FirstOrDefault(package => package.Equals(assemblyName))!;
-        }
+//        public static Package BuildPackage(this ProjectAssets projectAssets, string assemblyName, string targetFramework)
+//        {
+//            return projectAssets.BuildSerializablePackages(assemblyName, targetFramework)
+//                                .AsPackages()
+//                                .FirstOrDefault(package => package.Equals(assemblyName))!;
+//        }
 
-        public static IEnumerable<SerializablePackage> BuildSerializablePackages(this ProjectAssets projectAssets, string assemblyName, string targetFramework)
-        {
-            var assemblyConfigurationSection = projectAssets.GetTargetFrameworkTargets(targetFramework)!
-                                                            .GetChildren()
-                                                            .FirstOrDefault(child => child.Key.StartsWith(assemblyName!, StringComparison.OrdinalIgnoreCase));
-            var serializablePackage = new SerializablePackage
-            {
-                AssemblyName = assemblyConfigurationSection.Key,
-                DllRelativePath = assemblyConfigurationSection!.GetSection("runtime")?
-                                                               .GetChildren()
-                                                               .FirstOrDefault()?
-                                                               .Key!,
-                Dependencies = assemblyConfigurationSection!.GetSection("dependencies")?.GetChildren().Select(section => $"{section.Key}/{section.Value}").ToList()!
-            };
-            yield return serializablePackage;
-            foreach (var dependency in serializablePackage.Dependencies.SelectMany(depString => projectAssets.BuildSerializablePackages(depString, targetFramework)))
-            {
-                yield return dependency;
-            }
-        }
-    }
-}
+//        public static IEnumerable<SerializablePackage> BuildSerializablePackages(this ProjectAssets projectAssets, string assemblyName, string targetFramework)
+//        {
+//            var assemblyConfigurationSection = projectAssets.GetTargetFrameworkTargets(targetFramework)!
+//                                                            .GetChildren()
+//                                                            .FirstOrDefault(child => child.Key.StartsWith(assemblyName!, StringComparison.OrdinalIgnoreCase));
+//            var serializablePackage = new SerializablePackage
+//            {
+//                AssemblyName = assemblyConfigurationSection.Key,
+//                DllRelativePath = assemblyConfigurationSection!.GetSection("runtime")?
+//                                                               .GetChildren()
+//                                                               .FirstOrDefault()?
+//                                                               .Key!,
+//                Dependencies = assemblyConfigurationSection!.GetSection("dependencies")?.GetChildren().Select(section => $"{section.Key}/{section.Value}").ToList()!
+//            };
+//            yield return serializablePackage;
+//            foreach (var dependency in serializablePackage.Dependencies.SelectMany(depString => projectAssets.BuildSerializablePackages(depString, targetFramework)))
+//            {
+//                yield return dependency;
+//            }
+//        }
+//    }
+//}
