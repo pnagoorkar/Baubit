34e53f97467ecf4cdf6cda67bbd45c00a3de2e5f
prash <prashant.nagoorkar@gmail.com>
2024-10-26 12:19:54 -0400

    TraceabilityExtensions

---
+++ Baubit/Traceability/TraceabilityExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Baubit.Traceability
+{
+    public static class TraceabilityExtensions
+    {
+        public static void CaptureTraceEventIf(this object obj, bool condition, ITraceEvent traceEvent)
+        {
+            if (condition && obj is ITraceable traceable) traceable.History.Push(traceEvent);
+        }
+    }
+}
