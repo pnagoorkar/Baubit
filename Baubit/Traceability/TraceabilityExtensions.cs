4d0db04f3759f7793fb06ea4bf8bea2eff7c6955
prash <prashant.nagoorkar@gmail.com>
2024-10-27 09:08:38 -0400

    CaptureTraceEvent

--- Baubit/Traceability/TraceabilityExtensions.cs
+++ Baubit/Traceability/TraceabilityExtensions.cs
@@ -6,5 +6,15 @@
         {
             if (condition && obj is ITraceable traceable) traceable.History.Push(traceEvent);
         }
+
+        public static void CaptureTraceEvent(this object obj, ITraceEvent traceEvent)
+        {
+            if (obj is ITraceable traceable) traceable.CaptureTraceEvent(traceEvent);
+        }
+
+        public static void CaptureTraceEvent(this ITraceable traceable, ITraceEvent traceEvent)
+        {
+            if (traceable.EnableTrace) traceable.History.Push(traceEvent);
+        }
     }
 }
