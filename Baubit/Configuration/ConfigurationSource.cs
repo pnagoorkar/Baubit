ea121772321df02cb398d5ecb598605cff63419b
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-04-05 15:35:44 -0400

    fix #37

--- Baubit/Configuration/ConfigurationSource.cs
+++ Baubit/Configuration/ConfigurationSource.cs
@@ -19,110 +19,114 @@ namespace Baubit.Configuration
 
     public static class ConfigurationSourceExtensions
     {
-        /// <summary>
-        /// Loads an <see cref="IConfiguration"/> using the given <see cref="ConfigurationSource"/>
-        /// </summary>
-        /// <param name="configurationSource">An instance of <see cref="ConfigurationSource"/></param>
-        /// <returns>The built <see cref="IConfiguration"/></returns>
-        //public static IConfiguration Build(this ConfigurationSource configurationSource) => configurationSource.Build(null);
-
-        //public static IConfiguration Build(this ConfigurationSource configurationSource, IConfiguration configuration)
-        //{
-        //    if (configurationSource == null) return configuration;
-        //    var configurationBuilder = new ConfigurationBuilder();
-        //    configurationSource.AddJsonFiles(configurationBuilder).LoadResourceFiles().AddRawJsonStrings(configurationBuilder).AddSecrets(configurationBuilder);
-        //    if (configuration != null) configurationBuilder.AddConfiguration(configuration);
-        //    return configurationBuilder.Build();
-        //}
-
         public static Result<IConfiguration> Build(this ConfigurationSource configurationSource) => configurationSource.Build(null);
 
         public static Result<IConfiguration> Build(this ConfigurationSource configurationSource, IConfiguration configuration)
         {
             var configurationBuilder = new ConfigurationBuilder();
             return Result.OkIf(configurationSource != null, "")
-                         .Bind(() =>
-                         {
-                             configurationSource.AddJsonFiles(configurationBuilder)
-                                                .LoadResourceFiles()
-                                                .AddRawJsonStrings(configurationBuilder)
-                                                .AddSecrets(configurationBuilder);
-                             if (configuration != null) configurationBuilder.AddConfiguration(configuration);
-                             return Result.Ok<IConfiguration>(configurationBuilder.Build());
-                         });
+                         .Bind(() => configurationSource.AddJsonFiles(configurationBuilder))
+                         .Bind(configurationSource => configurationSource.LoadResourceFiles())
+                         .Bind(configurationSource => configurationSource.AddRawJsonStrings(configurationBuilder))
+                         .Bind(configurationSource => configurationSource.AddSecrets(configurationBuilder))
+                         .Bind(configurationSource => configurationBuilder.AddConfigurationToBuilder(configuration))
+                         .Bind(() => Result.Ok<IConfiguration>(configurationBuilder.Build()));
         }
 
-        private static ConfigurationSource AddJsonFiles(this ConfigurationSource configurationSource, ConfigurationBuilder configurationBuilder)
+        private static Result AddConfigurationToBuilder(this IConfigurationBuilder configurationBuilder, IConfiguration configuration)
         {
-            configurationSource?.ReplacePathPlaceholders(Application.Paths);
-            var jsonUris = configurationSource.JsonUriStrings.Select(uriString => new Uri(uriString));
-
-            foreach (var uri in jsonUris.Where(uri => uri.IsFile))
+            return Result.Try(() =>
             {
-                configurationBuilder.AddJsonFile(uri.LocalPath);
-            }
-            return configurationSource;
+                if (configuration != null) configurationBuilder.AddConfiguration(configuration);
+            });
         }
 
-        private static ConfigurationSource LoadResourceFiles(this ConfigurationSource configurationSource)
+        private static Result<ConfigurationSource> AddJsonFiles(this ConfigurationSource configurationSource, ConfigurationBuilder configurationBuilder)
         {
-            foreach (var embeddedJsonResource in configurationSource.EmbeddedJsonResources)
+            return Result.Try(() =>
             {
-                var identifierParts = embeddedJsonResource.Split(';');
-                var assemblyNamePart = identifierParts[0];
-                var fileNamePart = identifierParts[1];
+                configurationSource?.ReplacePathPlaceholders(Application.Paths);
+                var jsonUris = configurationSource.JsonUriStrings.Select(uriString => new Uri(uriString));
 
-                AssemblyName assemblyName;
-                if (assemblyNamePart.Contains("/"))
+                foreach (var uri in jsonUris.Where(uri => uri.IsFile))
                 {
-                    assemblyName = Reflection.AssemblyExtensions.GetAssemblyNameFromPersistableString(assemblyNamePart);
+                    configurationBuilder.AddJsonFile(uri.LocalPath);
                 }
-                else
+                return configurationSource;
+            });
+        }
+
+        private static Result<ConfigurationSource> LoadResourceFiles(this ConfigurationSource configurationSource)
+        {
+            return Result.Try(() =>
+            {
+                foreach (var embeddedJsonResource in configurationSource.EmbeddedJsonResources)
                 {
-                    assemblyName = new AssemblyName(assemblyNamePart);
+                    var identifierParts = embeddedJsonResource.Split(';');
+                    var assemblyNamePart = identifierParts[0];
+                    var fileNamePart = identifierParts[1];
+
+                    AssemblyName assemblyName;
+                    if (assemblyNamePart.Contains("/"))
+                    {
+                        assemblyName = Reflection.AssemblyExtensions.GetAssemblyNameFromPersistableString(assemblyNamePart);
+                    }
+                    else
+                    {
+                        assemblyName = new AssemblyName(assemblyNamePart);
+                    }
+                    var resourceName = $"{assemblyName.Name}.{fileNamePart}";
+
+                    var readResult = assemblyName.TryResolveAssembly()?.ReadResource(resourceName).GetAwaiter().GetResult();
+
+                    if (readResult?.IsSuccess != true) throw new Exception($"Failed to read embedded json resource {embeddedJsonResource}");
+                    configurationSource.RawJsonStrings.Add(readResult.Value);
                 }
-                var resourceName = $"{assemblyName.Name}.{fileNamePart}";
-
-                var readResult = assemblyName.TryResolveAssembly()?.ReadResource(resourceName).GetAwaiter().GetResult();
-
-                if (readResult?.IsSuccess != true) throw new Exception($"Failed to read embedded json resource {embeddedJsonResource}");
-                configurationSource.RawJsonStrings.Add(readResult.Value);
-            }
-            return configurationSource;
+                return configurationSource;
+            });
         }
 
-        private static ConfigurationSource AddRawJsonStrings(this ConfigurationSource configurationSource, ConfigurationBuilder configurationBuilder)
+        private static Result<ConfigurationSource> AddRawJsonStrings(this ConfigurationSource configurationSource, ConfigurationBuilder configurationBuilder)
         {
-            var memStreams = configurationSource?.RawJsonStrings.Select(rawJson => new MemoryStream(Encoding.UTF8.GetBytes(rawJson)));
-
-            foreach (var memStream in memStreams)
+            return Result.Try(() =>
             {
-                configurationBuilder.AddJsonStream(memStream);
-            }
+                var memStreams = configurationSource.RawJsonStrings.Select(rawJson => new MemoryStream(Encoding.UTF8.GetBytes(rawJson)));
 
-            return configurationSource;
-        }
-
-        private static ConfigurationSource AddSecrets(this ConfigurationSource configurationSource, ConfigurationBuilder configurationBuilder)
-        {
-            foreach(var localSecretsId in configurationSource.LocalSecrets)
-            {
-                configurationBuilder.AddUserSecrets(localSecretsId);
-            }
-
-            return configurationSource;
-        }
-
-        private static ConfigurationSource ReplacePathPlaceholders(this ConfigurationSource configurationSource, Dictionary<string, string> pathMap)
-        {
-            foreach (var kvp in pathMap)
-            {
-                for (int i = 0; i < configurationSource.JsonUriStrings.Count; i++)
+                foreach (var memStream in memStreams)
                 {
-                    configurationSource.JsonUriStrings[i] = Path.GetFullPath(configurationSource.JsonUriStrings[i].Replace(kvp.Key, kvp.Value));
+                    configurationBuilder.AddJsonStream(memStream);
                 }
-            }
-            return configurationSource;
+
+                return configurationSource;
+            });
+        }
+
+        private static Result<ConfigurationSource> AddSecrets(this ConfigurationSource configurationSource, ConfigurationBuilder configurationBuilder)
+        {
+            return Result.Try(() =>
+            {
+                foreach (var localSecretsId in configurationSource.LocalSecrets)
+                {
+                    configurationBuilder.AddUserSecrets(localSecretsId);
+                }
+
+                return configurationSource;
+            });
+        }
+
+        private static Result<ConfigurationSource> ReplacePathPlaceholders(this ConfigurationSource configurationSource, Dictionary<string, string> pathMap)
+        {
+            return Result.Try(() =>
+            {
+                foreach (var kvp in pathMap)
+                {
+                    for (int i = 0; i < configurationSource.JsonUriStrings.Count; i++)
+                    {
+                        configurationSource.JsonUriStrings[i] = Path.GetFullPath(configurationSource.JsonUriStrings[i].Replace(kvp.Key, kvp.Value));
+                    }
+                }
+                return configurationSource;
+            });
         }
     }
 }
