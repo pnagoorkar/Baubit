pnagoorkar/Baubit

commit 3c6e6adf4d3e98b3b5572f9d01a5032fb82da1ad

Revert "WIP - Improved validation and traceability overhaul"
This reverts commit 44da7a7bdf12a535f0bf3bf88c7956c44e7886b7.

Baubit.Test/Configuration/AConfiguration/Setup/DefaultValidator.cs
@@ -1,15 +1,0 @@
-﻿using Baubit.Validation;
-using FluentResults;
-using System.Linq.Expressions;
-
-namespace Baubit.Test.Configuration.AConfiguration.Setup
-{
-    [Validator(Key = "default")]
-    public class DefaultValidator : AValidator<Configuration>
-    {
-        protected override IEnumerable<Expression<Func<Configuration, Result>>> GetRules()
-        {
-            return [v => Result.Ok()];
-        }
-    }
-}

Baubit.Test/Configuration/AConfiguration/Test.cs
@@ -1,7 +1,4 @@
 ﻿using Baubit.Configuration;
-using Baubit.Configuration.Reasons;
-using Baubit.Traceability.Errors;
-using Baubit.Validation.Reasons;
 
 namespace Baubit.Test.Configuration.AConfiguration
 {
@@ -14,43 +11,9 @@ namespace Baubit.Test.Configuration.AConfiguration
             Environment.SetEnvironmentVariable("ENVIRONMENT", "Development");
             Environment.SetEnvironmentVariable("BaubitTestAssembly", "Baubit.Test");
             var configurationSource = new Baubit.Configuration.ConfigurationSource { EmbeddedJsonResources = [$"${{BaubitTestAssembly}};Configuration.AConfiguration.{fileName}"] };
-            var buildResult = configurationSource.Build().Bind(config => config.Load<Setup.Configuration>());
+            var buildResult = configurationSource.Build().Bind(config => config.Load<Configuration>());
             Assert.True(buildResult.IsSuccess);
             Assert.Equal("Development", buildResult.Value.CurrentEnvironment);
-        }
-
-        [Theory]
-        [InlineData("config.json")]
-        public void CanDetermineEnvVarNotFound(string fileName)
-        {
-            var configurationSource = new Baubit.Configuration.ConfigurationSource { EmbeddedJsonResources = [$"${{BaubitTestAssembly}};Configuration.AConfiguration.{fileName}"] };
-            var buildResult = configurationSource.Build().Bind(config => config.Load<Setup.Configuration>());
-            Assert.True(buildResult.IsFailed);
-            Assert.Contains(buildResult.Errors, error => error is CompositeError<string> compErr && compErr.NonErrorReasons.Any(reason => reason is EnvVarNotFound));
-        }
-
-        [Theory]
-        [InlineData("configWithOutValidator.json")]
-        public void CanValidateConfigurationOptionally(string fileName)
-        {
-            Environment.SetEnvironmentVariable("ENVIRONMENT", "Development");
-            Environment.SetEnvironmentVariable("BaubitTestAssembly", "Baubit.Test");
-            var configurationSource = new Baubit.Configuration.ConfigurationSource { EmbeddedJsonResources = [$"${{BaubitTestAssembly}};Configuration.AConfiguration.{fileName}"] };
-            var buildResult = configurationSource.Build().Bind(config => config.Load<Setup.Configuration>());
-            Assert.True(buildResult.IsSuccess);
-            Assert.Contains(buildResult.Reasons, reason => reason is ValidatorNotFound);
-        }
-
-        [Theory]
-        [InlineData("configWithValidator.json")]
-        public void CanValidateConfiguration(string fileName)
-        {
-            Environment.SetEnvironmentVariable("ENVIRONMENT", "Development");
-            Environment.SetEnvironmentVariable("BaubitTestAssembly", "Baubit.Test");
-            var configurationSource = new Baubit.Configuration.ConfigurationSource { EmbeddedJsonResources = [$"${{BaubitTestAssembly}};Configuration.AConfiguration.{fileName}"] };
-            var buildResult = configurationSource.Build().Bind(config => config.Load<Setup.Configuration>());
-            Assert.True(buildResult.IsSuccess);
-            Assert.Contains(buildResult.Successes, success => success is PassedValidation<Setup.Configuration> pass && pass.ValidationKey.Equals(buildResult.Value.ValidatorKey));
         }
     }
 }

Baubit/Configuration/AConfiguration.cs
@@ -7,7 +7,7 @@ namespace Baubit.Configuration
 {
     public abstract class AConfiguration : IValidatable
     {
-        public string ValidatorKey { get; init; }
+        public string ValidatorKey { get; init; } = "default";
     }
 
     public static class ConfigurationExtensions
@@ -16,7 +16,13 @@ namespace Baubit.Configuration
         {
             return Result.Try(() => iConfiguration.Get<TConfiguration>() ?? Activator.CreateInstance<TConfiguration>()!)
                          .Bind(config => config.ExpandURIs())
-                         .Bind(config => config.TryValidate(config.ValidatorKey, !string.IsNullOrEmpty(config.ValidatorKey?.Trim())));
+                         .Bind(config => config.Validate());
+        }
+        public static Result<TConfiguration> Validate<TConfiguration>(this TConfiguration configuration) where TConfiguration : AConfiguration
+        {
+            AValidator<TConfiguration> validator = null;
+            return Result.Try(() => AConfigurationValidator<TConfiguration>.CurrentValidators.TryGetValue(configuration.ValidatorKey, out validator))
+                         .Bind(_ => validator == null ? Result.Ok(configuration) : validator.Validate(configuration));
         }
 
         // convert AConfiguration to its most specific type and then serialize

Baubit/Configuration/ConfigurationSource.cs
@@ -1,9 +1,4 @@
-﻿using Baubit.Configuration.Exceptions;
-using Baubit.Configuration.Reasons;
-using Baubit.Reflection;
-using Baubit.Traceability;
-using Baubit.Traceability.Errors;
-using Baubit.Traceability.Exceptions;
+﻿using Baubit.Reflection;
 using FluentResults;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
@@ -45,42 +40,35 @@ namespace Baubit.Configuration
 
         public static Result<T> ExpandURIs<T>(this T obj)
         {
-            try
+            if (obj == null) return Result.Ok(obj);
+
+            var uriProperties = obj.GetType()
+                                   .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                   .Where(property => property.CustomAttributes.Any(att => att.AttributeType.Equals(typeof(URIAttribute))));
+
+            foreach (var uriProperty in uriProperties)
             {
-                if (obj == null) return Result.Ok(obj);
-
-                var uriProperties = obj.GetType()
-                                       .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                       .Where(property => property.CustomAttributes.Any(att => att.AttributeType.Equals(typeof(URIAttribute))));
-
-                foreach (var uriProperty in uriProperties)
+                if (uriProperty.PropertyType == typeof(string))
                 {
-                    if (uriProperty.PropertyType == typeof(string))
+                    var currentValue = (string)uriProperty.GetValue(obj);
+                    uriProperty.SetValue(obj, currentValue.ExpandURIString().Value);
+                }
+                else if (uriProperty.PropertyType == typeof(List<string>))
+                {
+                    var currentValues = (List<string>)uriProperty.GetValue(obj);
+                    if (currentValues.Count > 0)
                     {
-                        var currentValue = (string)uriProperty.GetValue(obj);
-                        uriProperty.SetValue(obj, currentValue.ExpandURIString().Value);
-                    }
-                    else if (uriProperty.PropertyType == typeof(List<string>))
-                    {
-                        var currentValues = (List<string>)uriProperty.GetValue(obj);
-                        if (currentValues.Count > 0)
-                        {
-                            var newValues = currentValues.Select(val => val.ExpandURIString().ThrowIfFailed().Value).ToList();
-                            uriProperty.SetValue(obj, newValues);
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("Unsupported URI property type!");
+                        var newValues = currentValues.Select(val => val.ExpandURIString().Value).ToList();
+                        uriProperty.SetValue(obj, newValues);
                     }
                 }
+                else
+                {
+                    throw new Exception("Unsupported URI property type!");
+                }
+            }
 
-                return Result.Ok(obj);
-            }
-            catch (FailedOperationException failedOpEx)
-            {
-                return Result.Fail("").WithReasons(failedOpEx.Result.Reasons);
-            }
+            return Result.Ok(obj);
 
         }
 
@@ -92,20 +80,8 @@ namespace Baubit.Configuration
             {
                 var key = match.Groups[1].Value;
                 var replacement = Environment.GetEnvironmentVariable(key);
-                return replacement == null ? throw new EnvironmentVariableNotFound(match.Value) : replacement;
-            }), HandleMissingEnvVariables);
-        }
-
-        private static IError HandleMissingEnvVariables(Exception exp)
-        {
-            if (exp is EnvironmentVariableNotFound envVarNotFoundExp)
-            {
-                return new CompositeError<string>([new EnvVarNotFound(envVarNotFoundExp.EnvVariable)], null, "", default);
-            }
-            else
-            {
-                return new ExceptionalError(exp);
-            }
+                return replacement ?? match.Value;
+            }));
         }
 
         private static Result AddConfigurationToBuilder(this IConfigurationBuilder configurationBuilder, IConfiguration configuration)

Baubit/Configuration/Exceptions/EnvironmentVariableNotFound.cs
@@ -1,11 +1,0 @@
-﻿namespace Baubit.Configuration.Exceptions
-{
-    public class EnvironmentVariableNotFound : Exception
-    {
-        public string EnvVariable { get; init; }
-        public EnvironmentVariableNotFound(string envVariable)
-        {
-            EnvVariable = envVariable;
-        }
-    }
-}

Baubit/DI/AConfiguration.cs
@@ -2,6 +2,6 @@
 {
     public abstract class AConfiguration : Configuration.AConfiguration
     {
-        public string ModuleValidatorKey { get; init; }
+        public string ModuleValidatorKey { get; init; } = "default";
     }
 }

Baubit/DI/ConfigurationExtensions.cs
@@ -2,7 +2,6 @@
 using Baubit.DI.Reasons;
 using Baubit.Reflection;
 using Baubit.Traceability.Errors;
-using Baubit.Validation;
 using FluentResults;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,7 +31,7 @@ namespace Baubit.DI
 
             var directlyDefinedModulesExtractionResult = configuration.GetModulesSectionOrDefault()
                                                                       .Bind(modulesSection => Result.Try(() => modulesSection?.GetChildren() ?? new List<IConfigurationSection>()))
-                                                                      .Bind(sections => Result.Merge(sections.Select(section => section.TryAsModule<TModule>()).ToArray()))
+                                                                      .Bind(sections => Result.Merge(sections.Select(section => section.TryAs<TModule>()).ToArray()))
                                                                       .Bind(modules => { directlyDefinedModules = modules.ToList(); return Result.Ok(); });
 
             var indirectlyDefinedModulesExtractionResult = configuration.GetModuleSourcesSectionOrDefault()
@@ -44,11 +43,6 @@ namespace Baubit.DI
             return directlyDefinedModulesExtractionResult.IsSuccess && indirectlyDefinedModulesExtractionResult.IsSuccess ?
                    Result.Ok<List<TModule>>([.. directlyDefinedModules, .. indirectlyDefinedModules]) :
                    Result.Fail(new CompositeError<IEnumerable<TModule>>(directlyDefinedModulesExtractionResult, indirectlyDefinedModulesExtractionResult));
-        }
-
-        public static Result<TModule> TryAsModule<TModule>(this IConfiguration configuration) where TModule : IModule
-        {
-            return configuration.TryAs<TModule>().Bind(module => module.TryValidate(module.Configuration.ModuleValidatorKey, !string.IsNullOrEmpty(module.Configuration.ModuleValidatorKey?.Trim())));
         }
 
         public static Result<T> TryAs<T>(this IConfiguration configuration)

Baubit/Traceability/Errors/AError.cs
@@ -17,8 +17,8 @@ namespace Baubit.Traceability.Errors
                          string message, 
                          Dictionary<string, object> metadata)
         {
-            NonErrorReasons = nonErrorReasons ?? new List<IReason>();
-            Reasons = reasons ?? new List<IError>();
+            NonErrorReasons = nonErrorReasons;
+            Reasons = reasons;
             Message = message;
             Metadata = metadata;
         }

commit b23cbb3dbe2828c345c61b37b1e1a9c0f0dbec01

Fixing the mess caused by 3c6e6adf4d3e98b3b5572f9d01a5032fb82da1ad

Baubit.Test/Configuration/AConfiguration/Setup/Configuration.cs
@@ -1,0 +1,10 @@
+﻿using Baubit.Configuration;
+
+namespace Baubit.Test.Configuration.AConfiguration.Setup
+{
+    public class Configuration : Baubit.Configuration.AConfiguration
+    {
+        [URI]
+        public string CurrentEnvironment { get; init; }
+    }
+}

Baubit.Test/Configuration/AConfiguration/Setup/DefaultValidator.cs
@@ -1,0 +1,15 @@
+﻿using Baubit.Validation;
+using FluentResults;
+using System.Linq.Expressions;
+
+namespace Baubit.Test.Configuration.AConfiguration.Setup
+{
+    [Validator(Key = "default")]
+    public class DefaultValidator : AValidator<Configuration>
+    {
+        protected override IEnumerable<Expression<Func<Configuration, Result>>> GetRules()
+        {
+            return [v => Result.Ok()];
+        }
+    }
+}

Baubit.Test/Configuration/AConfiguration/Test.cs
@@ -1,4 +1,4 @@
-using Baubit.Configuration;
+﻿using Baubit.Configuration;
 using Baubit.Configuration.Errors;
 using Baubit.Validation.Reasons;
 

Baubit/Configuration/AConfiguration.cs
@@ -7,7 +7,7 @@ namespace Baubit.Configuration
 {
     public abstract class AConfiguration : IValidatable
     {
-        public string ValidatorKey { get; init; } = "default";
+        public string ValidatorKey { get; init; }
     }
 
     public static class ConfigurationExtensions
@@ -16,13 +16,7 @@ namespace Baubit.Configuration
         {
             return Result.Try(() => iConfiguration.Get<TConfiguration>() ?? Activator.CreateInstance<TConfiguration>()!)
                          .Bind(config => config.ExpandURIs())
-                         .Bind(config => config.Validate());
-        }
-        public static Result<TConfiguration> Validate<TConfiguration>(this TConfiguration configuration) where TConfiguration : AConfiguration
-        {
-            AValidator<TConfiguration> validator = null;
-            return Result.Try(() => AConfigurationValidator<TConfiguration>.CurrentValidators.TryGetValue(configuration.ValidatorKey, out validator))
-                         .Bind(_ => validator == null ? Result.Ok(configuration) : validator.Validate(configuration));
+                         .Bind(config => config.TryValidate(config.ValidatorKey, !string.IsNullOrEmpty(config.ValidatorKey?.Trim())));
         }
 
         // convert AConfiguration to its most specific type and then serialize

Baubit/Configuration/ConfigurationSource.cs
@@ -1,4 +1,4 @@
-using Baubit.Configuration.Errors;
+﻿using Baubit.Configuration.Errors;
 using Baubit.Configuration.Exceptions;
 using Baubit.Reflection;
 using Baubit.Traceability;

Baubit/Configuration/Exceptions/EnvironmentVariableNotFound.cs
@@ -1,0 +1,11 @@
+﻿namespace Baubit.Configuration.Exceptions
+{
+    public class EnvironmentVariableNotFound : Exception
+    {
+        public string EnvVariable { get; init; }
+        public EnvironmentVariableNotFound(string envVariable)
+        {
+            EnvVariable = envVariable;
+        }
+    }
+}

Baubit/DI/AConfiguration.cs
@@ -2,6 +2,6 @@
 {
     public abstract class AConfiguration : Configuration.AConfiguration
     {
-        public string ModuleValidatorKey { get; init; } = "default";
+        public string ModuleValidatorKey { get; init; }
     }
 }

Baubit/DI/ConfigurationExtensions.cs
@@ -1,4 +1,4 @@
-using Baubit.Configuration;
+﻿using Baubit.Configuration;
 using Baubit.DI.Reasons;
 using Baubit.Reflection;
 using Baubit.Validation;

Baubit/Traceability/Errors/AError.cs
@@ -1,4 +1,4 @@
-using FluentResults;
+﻿using FluentResults;
 
 namespace Baubit.Traceability.Errors
 {

Baubit/Traceability/Successes/ASuccess.cs
@@ -1,0 +1,12 @@
+﻿using Baubit.Traceability.Reasons;
+using FluentResults;
+
+namespace Baubit.Traceability.Successes
+{
+    public abstract class ASuccess : AReason, ISuccess
+    {
+        protected ASuccess(string message, Dictionary<string, object> metadata) : base(message, metadata)
+        {
+        }
+    }
+}

Baubit/Traceability/TraceabilityExtensions.cs
@@ -1,4 +1,4 @@
-using Baubit.Traceability.Exceptions;
+﻿using Baubit.Traceability.Exceptions;
 using FluentResults;
 
 namespace Baubit.Traceability

Baubit/Validation/AValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentResults;
+﻿using Baubit.Validation.Reasons;
+using FluentResults;
 using System.Data;
 using System.Linq.Expressions;
 using System.Text;

Baubit/Validation/IValidatable.cs
@@ -1,4 +1,4 @@
-using Baubit.Traceability;
+﻿using Baubit.Traceability;
 using Baubit.Validation.Reasons;
 using FluentResults;
 

Baubit/Validation/Reasons/PassedValidation.cs
@@ -1,0 +1,13 @@
+﻿using Baubit.Traceability.Successes;
+
+namespace Baubit.Validation.Reasons
+{
+    public class PassedValidation<TValidatable> : ASuccess where TValidatable : IValidatable
+    {
+        public string ValidationKey { get; set; }
+        public PassedValidation(string validationKey) : base("Passed validation!", default)
+        {
+            ValidationKey = validationKey;
+        }
+    }
+}

Baubit.Test/Baubit.Test.csproj
@@ -20,6 +20,8 @@
   </ItemGroup>
 
   <ItemGroup>
+    <EmbeddedResource Include="Configuration\AConfiguration\configWithValidator.json" />
+    <EmbeddedResource Include="Configuration\AConfiguration\configWithOutValidator.json" />
     <EmbeddedResource Include="Configuration\AConfiguration\config.json" />
     <EmbeddedResource Include="Configuration\ConfigurationSource\config.json" />
     <EmbeddedResource Include="DI\ServiceProviderFactoryRegistrar\configWithDefaultServiceProviderFactory.json" />