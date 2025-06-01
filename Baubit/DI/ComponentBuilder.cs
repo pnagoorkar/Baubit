ac2d382948544acd8207ce071fe42153abef95c4
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-06-01 11:04:09 -0400

    ComponentBuilder to support building component container using Configured Service Provider Factory

--- Baubit/DI/ComponentBuilder.cs
+++ Baubit/DI/ComponentBuilder.cs
@@ -2,11 +2,10 @@
 using Baubit.DI.Reasons;
 using Baubit.Reflection;
 using Baubit.Traceability;
-using Baubit.Traceability.Exceptions;
-using Baubit.Validation;
 using FluentResults;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Reflection;
 
 namespace Baubit.DI
@@ -16,7 +15,6 @@ namespace Baubit.DI
         private IConfiguration _configuration;
         private List<Func<IServiceCollection, IServiceCollection>> _handlers = new List<Func<IServiceCollection, IServiceCollection>>();
         private bool _isDisposed;
-        private IServiceCollection _services = new ServiceCollection();
 
         private ComponentBuilder(IConfiguration configuration)
         {
@@ -43,31 +41,13 @@ namespace Baubit.DI
                                    .Bind(() => Result.Ok(this));
         }
 
-        public Result<ComponentBuilder<T>> WithServiceCollection(IServiceCollection services)
-        {
-            return Result.Try(() => _services = services).Bind(_ => Result.Ok(this));
-        }
-
         public Result<T> Build(bool requireComponent = true)
         {
-            return FailIfDisposed().Bind(() => Result.Try(() => _services ?? new ServiceCollection()))
-                                   .Bind(services => _handlers.Aggregate(Result.Ok<IServiceCollection>(services), (seed, next) => seed.Bind(s => Result.Try(() => next(s)))))
-                                   .Bind(services => CreateRootModule().Bind(rootModule => Result.Try(() => { rootModule.Load(services); return services; })))
-                                   .Bind(services => Result.Try(() => requireComponent ? services.BuildServiceProvider().GetRequiredService<T>() : services.BuildServiceProvider().GetService<T>()!))
-                                   .Bind(component => Result.Try(() => { Dispose(); return component; }));
-        }
-
-        private Result<IRootModule> CreateRootModule()
-        {
-            try
-            {
-                return RootModuleFactory.Create(_configuration)
-                                        .Bind(rootModule => rootModule.TryValidate(rootModule.Configuration.ModuleValidatorTypes));
-            }
-            catch(FailedOperationException failedOpEx)
-            {
-                return Result.Fail(string.Empty).WithReasons(failedOpEx.Result.Reasons);
-            }
+            return FailIfDisposed().Bind(() => Result.Try(() => Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings())))
+                                   .Bind(hostAppBuilder => Result.Try(() => hostAppBuilder.UseConfiguredServiceProviderFactory(_configuration)))
+                                   .Bind(hostAppBuilder => _handlers.Aggregate(Result.Ok(), (seed, next) => seed.Bind(() => Result.Try(() => { next(hostAppBuilder.Services); }))).Bind(() => Result.Ok(hostAppBuilder)))
+                                   .Bind(hostAppBuilder => Result.Try(() => hostAppBuilder.Build()))
+                                   .Bind(host => Result.Try(() => requireComponent ? host.Services.GetRequiredService<T>() : host.Services.GetService<T>()!));
         }
 
         private Result FailIfDisposed()
