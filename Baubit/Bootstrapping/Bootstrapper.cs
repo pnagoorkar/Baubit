a1cac187513bf3ce3afa25e20d0d90a067b03bce
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-09-21 10:37:46 -0400

    Bootstrapper module for bootstrapping objects

---
+++ Baubit/Bootstrapping/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Baubit.Bootstrapping
+{
+    public class Bootstrapper : IHostedService
+    {
+        public Bootstrapper(IEnumerable<IBootstrap> bootstraps)
+        {
+            
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Baubit/Bootstrapping/DI/Module.cs
@@ -0,0 +1,27 @@
+﻿using Baubit.Configuration;
+using Baubit.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Baubit.Bootstrapping.DI
+{
+    public class Module : AModule<Configuration>
+    {
+        public Module(ConfigurationSource configurationSource) : base(configurationSource)
+        {
+        }
+
+        public Module(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public Module(Configuration configuration, List<IModule> nestedModules, List<IConstraint> constraints) : base(configuration, nestedModules, constraints)
+        {
+        }
+
+        public override void Load(IServiceCollection services)
+        {
+            services.AddHostedService<Bootstrapper>();
+        }
+    }
+}
