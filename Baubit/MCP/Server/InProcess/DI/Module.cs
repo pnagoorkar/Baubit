5b7de9b20bffb47b60b1e98f0cfee493db8915b0
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-10-09 12:58:52 -0400

    Cleanup

--- Baubit/MCP/Server/InProcess/DI/Module.cs
+++ Baubit/MCP/Server/InProcess/DI/Module.cs
@@ -1,22 +1,12 @@
-﻿using Baubit.Bootstrapping;
-using Baubit.Configuration;
+﻿using Baubit.Configuration;
 using Baubit.MCP.Server.DI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using ModelContextProtocol.Client;
-using ModelContextProtocol.Protocol;
-using ModelContextProtocol.Server;
-using System.Net;
-using System.Net.Sockets;
 
 namespace Baubit.MCP.Server.InProcess.DI
 {
     public class Module<TServer> : AModule<Configuration>
     {
-        //Stream? serverSideStream = null;
-        //Stream? clientSideStream = null;
 
         public Module(ConfigurationSource configurationSource) : base(configurationSource)
         {
@@ -30,28 +20,6 @@ namespace Baubit.MCP.Server.InProcess.DI
         {
         }
 
-
-        //protected override void OnInitialized()
-        //{
-        //    CreateLoopbackStreams().Wait();
-        //}
-
-        //private async Task CreateLoopbackStreams()
-        //{
-        //    var listener = new TcpListener(IPAddress.Loopback, 0);
-        //    listener.Start();
-        //    var port = ((IPEndPoint)listener.LocalEndpoint).Port;
-
-        //    var client = new TcpClient();
-        //    var connectTask = client.ConnectAsync(IPAddress.Loopback, port);
-        //    var clientProxy = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
-        //    await connectTask.ConfigureAwait(false);
-        //    listener.Stop();
-
-        //    serverSideStream = clientProxy.GetStream();
-        //    clientSideStream = client.GetStream();
-        //}
-
         public override void Load(IServiceCollection services)
         {
             services.AddSingleton<McpServer>();
@@ -63,23 +31,5 @@ namespace Baubit.MCP.Server.InProcess.DI
         {
             return serviceProvider.GetRequiredService<McpServer>().InternalServer;
         }
-
-        //protected override ITransport BuildServerTransport(IServiceProvider serviceProvider)
-        //{
-        //    return new StreamServerTransport(serverSideStream, serverSideStream, Configuration.ServerName, serviceProvider.GetRequiredService<ILoggerFactory>());
-        //}
-
-        //private async Task<McpClient> BuildClientAsync(IServiceProvider serviceProvider)
-        //{
-        //    return await McpClient.CreateAsync(BuildStreamClientTransport(serviceProvider), 
-        //                                       serviceProvider.GetRequiredService<IOptions<McpClientOptions>>().Value, 
-        //                                       serviceProvider.GetRequiredService<ILoggerFactory>())
-        //                           .ConfigureAwait(false);
-        //}
-
-        //private StreamClientTransport BuildStreamClientTransport(IServiceProvider serviceProvider)
-        //{
-        //    return new StreamClientTransport(clientSideStream, clientSideStream, serviceProvider.GetRequiredService<ILoggerFactory>());
-        //}
     }
 }
